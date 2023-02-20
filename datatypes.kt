ba430831a1676fab4ee5691c08f3d9bce314ea4a
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2023-02-20 17:44:53 +0530

    Integer datatypes now with Max and Min Value of its datatypes

--- datatypes.kt
+++ datatypes.kt
@@ -7,4 +7,6 @@ fun main() {
     val maxInt: Int=Int.MAX_VALUE
     val minInt: Int=Int.MIN_VALUE
     println("Max Value= $maxInt ,Min Value= $minInt")
+
+    // val heli:Int=214748648 this make the program led to error because it exceed the limit of integer data type
 }
