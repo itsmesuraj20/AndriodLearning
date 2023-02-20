38f12bc37dc49de6ddb3d7c079f4c5b9233bae9a
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2023-02-20 16:37:06 +0530

    operator learning in Kotlin

--- operator.kt
+++ operator.kt
@@ -8,4 +8,13 @@ fun main()
     println("x * y= ${x*y}")//Placeholder technique syntax (${.................})
     println("x / y= ${x/y}")//Placeholder technique syntax (${.................})
     println("x % y= ${x % y}")//Placeholder technique syntax (${.................})
+
+
+
+
+    var x1=0
+    var x2=0 
+    println("x= ${x1++} ,${++x2}")
+    println("x=${++x1}, ${x2++}, ${x2}")
+    //val cannot be reassigned means that val datatypes cant change its value under any circumstances
 }
