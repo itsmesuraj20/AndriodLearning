399bf44b1d018405ed9fba0e6a69514834d659d7
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2023-02-20 17:38:07 +0530

    Integer datatypes

--- datatypes.kt
+++ datatypes.kt
@@ -1,6 +1,10 @@
 fun main() {
-    var a=5
-    val b=6
-    println("Sum of two no.= ${x + y}")
-    
+    var name:String="Suraj Singh"
+    val age:Int=20 // syntax of intizalization
+    println("Name= $name ,age= $age")
+
+
+    val maxInt: Int=Int.MAX_VALUE
+    val minInt: Int=Int.MIN_VALUE
+    println("Max Value= $maxInt ,Min Value= $minInt")
 }
