7f108113fdff77ff8b996248a6721678c26b8131
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2023-02-20 16:12:04 +0530

    operator learning in kotlin #Day3

---
+++ datatypes.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var a=5
+    val b=6
+    println("Sum of two no.= ${x + y}")
+    
+}
---
+++ operator.kt
@@ -0,0 +1,11 @@
+fun main()
+{
+    var x=5
+    val y=2.51
+
+    println("x + y= ${x+y}")//Placeholder technique syntax (${.................})
+    println("x - y= ${x-y}")//Placeholder technique syntax (${.................})
+    println("x * y= ${x*y}")//Placeholder technique syntax (${.................})
+    println("x / y= ${x/y}")//Placeholder technique syntax (${.................})
+    println("x % y= ${x % y}")//Placeholder technique syntax (${.................})
+}
