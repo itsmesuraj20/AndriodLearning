147ff8a7283f5fdffb91cd24919aa9a7afcc3d25
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2023-02-20 18:47:55 +0530

    when and In Control Flow learning

---
+++ whencontrolflow.kt
@@ -0,0 +1,19 @@
+fun main()
+{
+    val al=15;
+    
+    when(al){
+        12 -> println("The time is $al")
+        7 -> println("The time is $al")
+        14 -> println("The time is $al")
+        else -> println("The time is $al")
+        
+    }
+
+    val a=8
+
+    when(a){
+        12,7,8 ->println("The day is $a")
+        else ->println("The day is day $a")
+    }
+}
